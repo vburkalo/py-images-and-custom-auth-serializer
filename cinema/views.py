c6fbddfb0c10a30b108dda8c378dd56b16e19432
vital <vitaliyburkalo81@gmail.com>
2024-05-18 14:04:43 +0300

    refactored upload image function to automactically raise exception

--- cinema/views.py
+++ cinema/views.py
@@ -111,11 +111,10 @@ class MovieViewSet(
             data=request.data,
             partial=True
         )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def get_serializer_class(self):
         if self.action == "list":
